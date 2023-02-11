57b2039a6192202684ff35ca9869817df2b27695
saliei <saeid.aliei@gmail.com>
2023-02-11 23:17:20 +0330

    fix colors and dimensions

--- include/args.sh
+++ include/args.sh
@@ -5,34 +5,31 @@ CFDISK="/usr/bin/cfdisk"
 CGDISK="/usr/bin/cgdisk"
 DHCPCD="/usr/sbin/dhcpcd"
 
-export DIALOG_ESC=1
-export DIALOG_EXTRA=255
-export DIALOG_OPTS="--keep-tite --colors \
-    --backtitle \"The Interactive Linux Installer\" --cancel-label \"Back\""
+#export DIALOG_ESC=1
+#export DIALOG_EXTRA=255
+# TODO: maybe a better way to export this
+export DIALOGRC="$PWD/dialogrc"
 
-# hardcoded color codes,
-# also `tput setaf` can be used for a more dynamic version of color codes, 
-# e.g. BLACK=$(tput setaf 30)
-BLACK="\e[0;90m"
-RED="\e[0;91m"
-GREEN="\e[0;92m"
-BROWN="\e[0;93m"
-BLUE="\e[0;94m"
-PURPLE="\e[0;95m"
-CYAN="\e[0;96m"
-GREY="\e[0;97m"
-BOLD="\e[1m"
-ULINE="\e[4m"
-EXPANDBG="\e[K"
-BLUEBG="\e[0;104m"
-BLUEBG_EXPAND="${BLUEBG}${EXPANDBG}"
-RESET="\e[0m"
+# color codes,
+BLACK="\Z0"
+RED="\Z1"
+GREEN="\Z2"
+YELLOW="\Z3"
+BLUE="\Z4"
+PURPLE="\Z5"
+CYAN="\Z6"
+GREY="\Z7"
+BOLD="\Zb"
+BBOLD=${BOLD}${BLUE}
+ULINE="\Zu"
+RESET="\Zn"
 
 # dialog sizes
-INPUT_SIZE="8 60"
-MSGBOX_SIZE="8 70"
+INPUT_SIZE="10 65"
+MSGBOX_SIZE="10 70"
 MENU_SIZE="14 60 0"
-YESNO_SIZE="$INPUTSIZE"
+YESNO_SIZE="$INPUT_SIZE"
 WIDGET_SIZE="10 70"
 MENU_LABEL="Use ${BOLD}UP${RESET} and ${BOLD}DOWN${RESET} keys to navigate menus. \
     Use ${BOLD}TAB${RESET} to switch between buttons and ${BOLD}ENTER${RESET} to select."
+
